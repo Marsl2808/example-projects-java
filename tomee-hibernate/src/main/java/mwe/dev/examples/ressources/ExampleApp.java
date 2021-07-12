33aa5c1fa64d49544a0f16274f99abb2e393856b
Marcel Wendling <m.wendling2808@gmx.de>
2021-07-12 20:54:46 +0200

    tomee hibernate example added

---
+++ tomee-hibernate/src/main/java/mwe/dev/examples/ressources/ExampleApp.java
@@ -0,0 +1,57 @@
+package mwe.dev.examples.ressources;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Calendar;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import de.mwe.dev.examples.model.Geschlecht;
+import de.mwe.dev.examples.model.Person;
+import de.mwe.dev.examples.model.PersonDAO;
+
+/**
+ *
+ */
+@Path("example")
+@Singleton
+@Timed
+public class ExampleApp {
+
+    @GET
+    @Path("/hello")
+    public String sayHello() {
+        return "["
+        		+ "{"
+        		+ "\"first:\": Hello,"
+        		+ "\"second\": World"
+        		+ "}"        		
+        		+"]";
+    }
+    
+    @GET
+    @Path("/testdb")
+    public String testDb() {
+    	
+		PersonDAO personDao = new PersonDAO();
+		Person p = new Person();
+		
+		p.setVorname("Max");
+		p.setNachname("Mustermann");
+		
+		p.setGeschlecht(Geschlecht.MAENNLICH);
+		
+		p.setKommentar("Testkommentar_neu");
+		
+		personDao.persist(p);
+				
+		personDao.shutdown();
+    	
+    	return "test";
+    }
+}
---
+++ tomee-hibernate/src/main/java/mwe/dev/examples/ressources/ExampleRessource.java
@@ -0,0 +1,11 @@
+package mwe.dev.examples.ressources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/")
+public class ExampleRessource extends Application {
+}
