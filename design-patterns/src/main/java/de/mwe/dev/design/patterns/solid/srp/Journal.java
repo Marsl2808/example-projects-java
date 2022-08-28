24eb87eabdba0d6a32a9fc795b8f53e825c5c4e2
Marsl <m.wendling2808@gmx.de>
2022-08-28 10:33:13 +0200

    (design patterns) removing equals method

--- design-patterns/src/main/java/de/mwe/dev/design/patterns/solid/srp/Journal.java
+++ design-patterns/src/main/java/de/mwe/dev/design/patterns/solid/srp/Journal.java
@@ -28,21 +28,4 @@ public class Journal {
 	public String toString() {
 		return String.join(System.lineSeparator(), entries);
 	}
-	
-	@Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (obj.getClass() != this.getClass()) {
-            return false;
-        }
-
-        final Journal other = (Journal) obj;
-        if ((this.entries == null) ? (other.entries != null) : !this.entries.equals(other.entries)) {
-            return false;
-        }
-        return true;
-    }
 }
