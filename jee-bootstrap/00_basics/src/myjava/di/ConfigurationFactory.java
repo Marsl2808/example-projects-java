3094e4863b42d0688e35b955b3e0a8c7132437ae
Marslinho <m.wendling2808@gmx.de>
2024-03-19 20:29:47 +0100

    rm .class

--- jee-bootstrap/00_basics/src/myjava/di/ConfigurationFactory.java
+++ jee-bootstrap/00_basics/src/myjava/di/ConfigurationFactory.java
@@ -1,20 +1,28 @@
 package myjava.di;
 
 public class ConfigurationFactory {
-  
-  private final static ConfigurationFactory INSTANCE = new ConfigurationFactory();
+
+  private static final ConfigurationFactory INSTANCE = new ConfigurationFactory();
 
   private Configuration product;
 
-  private ConfigurationFactory(){
-    this.product = new SystemPropertyConfiguration();
+  private ConfigurationFactory() {
+    // first aproach -> ConfigurationFactory needs to be recompiled
+    // this.product = new SystemPropertyConfiguration();
+
+    // 2nd approach -> load class by configurable String (typ. String is read from XML)
+    try {
+      this.product = (Configuration) Class.forName("myjava.di.SystemPropertyConfiguration").newInstance();
+    } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+      throw new IllegalStateException("Product not found!", e);
+    }
   }
 
-  public Configuration create(){
+  public Configuration create() {
     return this.product;
   }
 
-  public static final ConfigurationFactory getInstance(){
+  public static final ConfigurationFactory getInstance() {
     return INSTANCE;
   }
 }
